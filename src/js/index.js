fec9738c8ccfffc321484a5552cdbaef995f4d56
kkateli <kateli24@outlook.com>
2019-04-15 19:48:49 +1200

    Bundled html

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 //only the name of the file
 import s from "./testing";
-console.log("I have imported "+ s + " from testing.js");
+console.log("I have imported "+ s + " from the file testing.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
  //retrieve absolute path
  const path = require('path');
+ //retrieve html webpack plugin
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+  
  
  //export configuration object we write, so that webpack can work with it
  module.exports = {
@@ -8,10 +11,23 @@
      //where to save bundle file
      output:{
          //needs,absolute path, dirname is the path we retrieve, followed ny the location we want to bundle in
-         path: path.resolve(__dirname, 'dist/js'),
-         filename: 'bundle.js'//standard name for webpack output 
+         path: path.resolve(__dirname, 'dist'),
+         filename: 'js/bundle.js'//standard name for webpack output 
      },
-     mode: 'development'
+     devServer:{
+         contentBase:'./dist'
+     },
+     //receive an array containing all the plugins we are using
+     //here we wan to grab/bundel index.html in the scr folder to dist
+     plugins:[
+         new HtmlWebpackPlugin({
+             fielname: 'index.html',
+             template: './src/index.html'//starting file
 
+
+         })
+
+        ]
+    
 
  };
